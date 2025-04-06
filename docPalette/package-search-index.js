e68db9fb803618960f7012045d513863ab09cac2
strouilhet <sylvie.trouilhet@irit.fr>
2025-04-06 19:21:33 +0200

    maj doc Palette

--- docPalette/package-search-index.js
+++ docPalette/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"main"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"r203Tp2bis"}];updateSearchResults();
--- docPalette/type-search-index.js
+++ docPalette/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Artiste"},{"p":"main","l":"Couleur"},{"p":"main","l":"Palette"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"r203Tp2bis","l":"Artiste"},{"p":"r203Tp2bis","l":"Couleur"},{"p":"r203Tp2bis","l":"Palette"}];updateSearchResults();
