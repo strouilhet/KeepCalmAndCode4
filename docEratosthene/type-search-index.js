3f93dac770fb6ef817a3c12da64de8942742e372
strouilhet <sylvie.trouilhet@irit.fr>
2025-04-06 18:35:32 +0200

    maj doc crible

--- docEratosthene/type-search-index.js
+++ docEratosthene/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"CribleEratosthene"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"r203Tp2","l":"CribleEratosthene"},{"p":"r203Tp2","l":"GenerateurTicketsGagnants"}];updateSearchResults();
